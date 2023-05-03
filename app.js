aca5b70970f4df2860e787b2d928158589c632b0
MaiCodes-exe <maise.b.costa@gmail.com>
2023-05-03 19:53:48 +0100

    added eeven listener to navbar

--- app.js
+++ app.js
@@ -9,4 +9,13 @@ const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
 
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for(let i = 0; i < elemArr.length; i++){
+    elemArr[i].addEventListener("click", function () {
+        navbar.classList.toggle("active")
+        overlay.classList.toggle("active")
+
+    })
+}
 
