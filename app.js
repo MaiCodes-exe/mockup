d0183b1d51cb2a18b65ce3a92f796128ac3ff609
MaiCodes-exe <maise.b.costa@gmail.com>
2023-05-03 20:13:20 +0100

    changed directions

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// "use strict";
+"use strict";
 
 
-// // navbar toggle
+// navbar toggle
 
-// const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-// const navbar = document.querySelector("[data-navbar]");
-// const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-// const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
 
 
-// const elemArr = [navCloseBtn, overlay, navOpenBtn];
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
 
-// for(let i = 0; i < elemArr.length; i++){
-//     elemArr[i].addEventListener("click", function () {
-//         navbar.classList.toggle("active")
-//         overlay.classList.toggle("active")
+for(let i = 0; i < elemArr.length; i++){
+    elemArr[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
 
-//     })
-// }
+    });
+}
+
 
