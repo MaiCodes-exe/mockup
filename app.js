8733a904f574acb655a0d8214afa85c9326dc645
MaiCodes-exe <maise.b.costa@gmail.com>
2023-05-17 17:50:21 +0100

    added function to scroll up

--- app.js
+++ app.js
@@ -15,3 +15,9 @@ navToggleBtn.addEventListener("click", function () {
   
 
 
+  const goTopBtn = document.querySelector("[data-go-top]");
+
+  window.addEventListener("scroll", function () {
+    window.scrollY >= 500 ? goTopBtn.classList.add("active")
+      : goTopBtn.classList.remove("active");
+  });
